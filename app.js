612f5c95e09f07dfc357352005b6f8449cdbef49
OtariOboladze <otari.oboldze@example.com>
2020-05-12 18:29:47 +0200

    second version

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-var slideIndex = 1;
+let slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+	showDivs((slideIndex += n));
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
+	let i;
+	let x = document.getElementsByClassName("mySlides");
+	if (n > x.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = x.length;
+	}
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	x[slideIndex - 1].style.display = "block";
 }
